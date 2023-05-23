2f65488281040a5c3d5a4a116f924b3f692f9545
Paulla0220 <131075772+Paulla0220@users.noreply.github.com>
2023-05-23 17:21:26 +0000

    wyres gotowy

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -4,13 +4,24 @@ class ChartController < ApplicationController
     def index
         @user = get_current_logged_in_user
         @pumpups = @user.pumpups
-        @chart = getChart
+        @chartRep = getChartRep
+        @chartSeries = getChartSeries
+        @chartDate = getChartDate
     end
 
     private
 
-    def getChart
-        @pumpups.pluck(:data, :rep)
+    def getChartRep
+        @pumpups.order(date: :asc).pluck(:date, :rep).map { |date, value| [date.strftime("%d.%m.%Y"), value] }
+    end
+
+    def getChartSeries
+        @pumpups.order(date: :asc).pluck(:date, :series).map { |date, value| [date.strftime("%d.%m.%Y"), value] }
+    end
+
+    def getChartDate
+        multiply = @pumpups.order(date: :asc).pluck(:date, :rep, :series).map { |date, value1, value2| [date, value1 * value2] }
+        getDate = multiply.map { |date, value| [date.strftime("%d.%m.%Y"), value] }
     end
 
     def get_current_logged_in_user
