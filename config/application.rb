8dc58ca19b76b55f5f1a8d9e2e07b7f73bf2f5c5
Isia07 <91089566+Isia07@users.noreply.github.com>
2023-06-06 23:49:05 +0000

    tłumaczenia

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,20 @@ module CodespacesTryRails
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+
+    # Ustaw domyślny język
+    config.i18n.default_locale = :pl
+
+    # Dostępne języki
+    config.i18n.available_locales = [:pl, :en]
+
+    # Ścieżki, w których znajdują się pliki tłumaczeń
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+
+    # Ustaw strefę czasową
     config.time_zone = "Warsaw"
+
+    # Inne konfiguracje...
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
