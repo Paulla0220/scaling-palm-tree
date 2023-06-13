0fafad3981eea51fba3b0796c6659b2a60d8e4d8
Paulla0220 <131075772+Paulla0220@users.noreply.github.com>
2023-06-13 18:36:45 +0000

    name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :pumpups
+  
+  validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "może zawierać tylko litery" }
+  validates :surname, format: { with: /\A[a-zA-Z]+\z/, message: "może zawierać tylko litery" }
 end
